5d9f498cc0dfe242051f8a3e158cd247842d3759
Seth Adamson <seth.c.adamson@gmail.com>
2018-07-24 00:43:53 -0600

    mentorTest updates

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,58 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt: function (str){
+    return str.split('').reverse().join('');
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups: function (arr){
+    let newArr = [];
+    arr.forEach(e => {
+      if(!newArr.includes(e))
+        newArr.push(e);
+    });
+    return newArr;
+  },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt: function (str){
+    return str.split(' ').map(e => e.charAt(0).toUpperCase() + e.slice(1)).join(' ');
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+  vowelCounter: function(str){
+    let vowels = ['a','e','i','o','u'];
+    let count = 0;
+    str.split('').forEach(e => {
+      if(vowels.includes(e)){
+        count++;
+      }
+    });
+    return count;
+  },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime: function(num){
+    var val = Math.abs(num);
+    if(val === 1){
+      return false;
+    }
+    for(let i = 2; i < val; i++){
+      if(val % i === 0){
+        return false;
+      }
+    }
+    return true;
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +61,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: '1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +72,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+  log1: 'Hello World',
+  log2: undefined,
 }
